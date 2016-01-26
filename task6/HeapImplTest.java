781bbbb264e11d37a9dcb39a944906a6f181e172
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-01-26 21:27:45 +0600

    test extractMax and add heap's methods

--- task6/HeapImplTest.java
+++ task6/HeapImplTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -8,8 +9,17 @@ import static org.junit.Assert.assertEquals;
 public class HeapImplTest {
     @Test
     public void testExtractMax() throws Exception {
-        List<Integer> arr = Arrays.asList(1, 6, 3);
+        final List<Integer> arr = Arrays.asList(1, 6, 3);
         final HeapImpl heap = new HeapImpl(arr);
+        final int max = heap.extractMax();
+        assertEquals(6, max);
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        final List<Integer> arr = Collections.singletonList(1);
+        final HeapImpl heap = new HeapImpl(arr);
+        heap.add(6);
         final int max = heap.extractMax();
         assertEquals(6, max);
     }
