009238ce2967df6ec2fe65dad935585d80aeb4d2
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-01-27 21:21:03 +0600

    create AlgorithmData class for task6

---
+++ task6/AlgorithmData.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AlgorithmData {
+    public final List<Integer> fruitMasses;
+    public final int K;
+
+    public AlgorithmData(Scanner sc) {
+        fruitMasses = new ArrayList<>();
+        final int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            fruitMasses.add(sc.nextInt());
+        }
+        K = sc.nextInt();
+    }
+}
