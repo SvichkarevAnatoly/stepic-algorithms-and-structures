491f2b7908b2a1e3367d6145c65d8ef1a4c74f48
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-01-31 03:14:45 +0600

    feature constructor with args string

--- task6/AlgorithmData.java
+++ task6/AlgorithmData.java
@@ -3,10 +3,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AlgorithmData {
-    public final List<Integer> fruitMasses;
-    public final int K;
+    public List<Integer> fruitMasses;
+    public int K;
+
+    public AlgorithmData(String dataStr) {
+        final Scanner sc = new Scanner(dataStr);
+        parse(sc);
+    }
 
     public AlgorithmData(Scanner sc) {
+        parse(sc);
+    }
+
+    private void parse(Scanner sc) {
         fruitMasses = new ArrayList<>();
         final int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
